ae499f5db78543938590c2c430568b854d43a01e
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-02-23 00:36:13 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 67.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list) {
+        
+        int[] result = new int[2];
+        int even = 0;
+        int odd = 0;
+        
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        result[0] = even;
+        result[1] = odd;
+        
+        return result;
+    }
+}
