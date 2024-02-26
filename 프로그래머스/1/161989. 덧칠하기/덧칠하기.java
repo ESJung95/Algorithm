08b59a132e58f6620b0c6f652dea0acdb51e7028
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-02-26 18:20:21 +0900

    [level 1] Title: 덧칠하기, Time: 1.03 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,16 @@
+        class Solution {
+            public int solution(int n, int m, int[] section) {
+                int result = 0;
+                int start = section[0];
+                
+                result++;
+                for (int item : section) {
+                    if (start + m > item) {
+                        continue;
+                    }
+                    start = item;
+                    result++;
+                }
+                return result;
+            }
+        }
