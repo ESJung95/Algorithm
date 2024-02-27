84380158230889294f0bb80945b40d9495556654
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-02-27 16:16:16 +0900

    [level 0] Title: 문자열 밀기, Time: 1.56 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,45 +1,7 @@
-import java.util.*;
+
 class Solution {
     public int solution(String A, String B) {
-        
-        String[] arr = new String[A.length()];
-        int index = 0;
-        for (String item : A.split("")) {
-            arr[index] = item;
-            index++;
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        int result = -1;
-        
-        if (A.equals(B)) {
-            return 0;
-        }
-        
-        int cnt = 0;
-        while(cnt <= arr.length - 1) {
-            
-            String temp = arr[arr.length - 1];
-            sb.append(temp);
-            
-            for (int i = 0; i < arr.length - 1; i++) {
-                sb.append(arr[i]);
-            }
-            
-            cnt++;
-            if (B.equals(sb.toString())) {
-            result = cnt;
-            break;
-            }
-            
-            index = 0;
-            for (String item : sb.toString().split("")) {
-                arr[index] = item;
-                index++;
-            }
-            
-            sb.setLength(0);
-        }
-        return result;
+
+        return (B+B).indexOf(A);
     }
 }
