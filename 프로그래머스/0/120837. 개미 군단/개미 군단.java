5a6fdf563e15256ade7d475449a357ad49afe7bf
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-03-03 05:17:48 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int hp) {
+        
+        int cnt = 0;
+        while(hp > 0) {
+            if (hp >= 5) {
+                hp -= 5;
+                cnt++;
+            } else if (hp >= 3) {
+                hp -= 3;
+                cnt++;
+            } else {
+                hp -= 1;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
