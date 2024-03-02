53c3201b3a02fce23e6d7f5227ab87b7e8b00fca
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-03-02 23:25:19 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.02 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -2,18 +2,19 @@ import java.util.*;
 class Solution {
     public String[] solution(String my_string) {
         
-        ArrayList<String> temp  = new ArrayList<>();
-        for (String item : my_string.split(" ")) {
-            temp.add(item);
-        }
+//         ArrayList<String> temp  = new ArrayList<>();
+//         for (String item : my_string.split(" ")) {
+//             temp.add(item);
+//         }
         
-        String[] result = new String[temp.size()];
-        int index = 0;
-        for (String item : temp) {
-            result[index] = item;
-            index++;
-        }
+//         String[] result = new String[temp.size()];
+//         int index = 0;
+//         for (String item : temp) {
+//             result[index] = item;
+//             index++;
+//         }
         
+        String[] result = my_string.split(" ");
         return result;
     }
 }
