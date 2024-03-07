9cc3c164d66b9e5015a7f086ea9e7ed38dc70963
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-03-07 22:50:00 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public String solution(String n_str) {
+        
+        int temp =  Integer.parseInt(n_str);
+        String result = String.valueOf(temp);
+        return result;
+    }
+}
