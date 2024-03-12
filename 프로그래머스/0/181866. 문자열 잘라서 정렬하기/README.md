d11f9d880452ad2a9b640ebe19bf528e5747a320
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-03-13 02:08:37 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 15.23 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/README.md
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 잘라서 정렬하기 - 181866 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181866) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181866#) 
 
 ### 성능 요약
 
-메모리: 95 MB, 시간: 48.44 ms
+메모리: 94.1 MB, 시간: 15.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 13일 01:56:06
+2024년 03월 13일 02:08:35
 
 ### 문제 설명
 
--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -1,35 +1,25 @@
 import java.util.*;
+
 class Solution {
     public String[] solution(String myString) {
+        
+        String[] split = myString.split("x");
+        ArrayList<String> list = new ArrayList <> ();
 
-        String[] arr = new String[myString.length() + 1];
-        int index = 0;
-        for (String item : myString.split("")) {
-            arr[index] = item;
-            index++;
-        }
-        arr[arr.length - 1] = "x";
-        ;
-        ArrayList<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < arr.length; i++) {
-            if (!arr[i].equals("x")) {
-                sb.append(arr[i]);
-            } else {
-                if (!sb.toString().equals("")) {
-                    list.add(sb.toString());
-                }
-                sb.setLength(0);
+        for(int i = 0; i < split.length; i ++){
+            if(!split[i].equals("")){
+                list.add(split[i]);
             }
-        }
 
+        }
+    
         String[] result = new String[list.size()];
-        for (int i = 0; i < result.length; i++) {
+        for(int i = 0; i < result.length; i ++){
             result[i] = list.get(i);
         }
-        
         Arrays.sort(result);
+
+
         return result;
     }
 }
