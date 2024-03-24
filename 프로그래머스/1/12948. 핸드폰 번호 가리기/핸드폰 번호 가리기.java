8bf58d57fa9f3f072b23c8f8d6769fddbbfadfd7
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-03-24 23:48:07 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public String solution(String phone_number) {
+
+        String last = phone_number.substring(phone_number.length() - 4);
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < phone_number.length() - 4; i++) {
+            sb.append("*");
+        }
+        sb.append(last);
+        return sb.toString();
+    }
+}
