905ce64906fbdb24862b6b797f6b156c7a71dc59
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-03-26 22:08:13 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,22 +2,24 @@ import java.util.*;
 class Solution {
     
     static boolean[] visited;
-        static int max = 0;
+    static int max = 0;
+    
     public void dfs(int k, int[][] dungeons, int cnt) {
-        
         for (int i = 0; i < dungeons.length; i++) {
             int min = dungeons[i][0];
             int use = dungeons[i][1];
             
             if (!visited[i] && k >= min) {
                 visited[i] = true;
+                
                 dfs(k - use, dungeons, cnt+1);
                 
                 // 다른 케이스를 위해서 방문 취소 처리
                 visited[i] = false;
             }
-            max = Math.max(max, cnt);
         }
+        
+        max = Math.max(max, cnt);
     }
     
     public int solution(int k, int[][] dungeons) {
@@ -25,7 +27,6 @@ class Solution {
         
         dfs(k, dungeons, 0);
         return max;
-        
         
     }
 }
