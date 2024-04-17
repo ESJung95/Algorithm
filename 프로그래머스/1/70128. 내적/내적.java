5467d86dc20b7914ca222b995f797aac4a2c40da
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-04-17 19:49:14 +0900

    [level 1] Title: 내적, Time: 0.06 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -2,8 +2,7 @@ class Solution {
     public int solution(int[] a, int[] b) {
         
         int sum = 0;
-        int length = a.length;
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < a.length; i++) {
             sum += (a[i] * b[i]);
         }
         
