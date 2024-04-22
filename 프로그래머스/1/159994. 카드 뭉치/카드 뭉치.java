d39b5ce35dc66d94abae74eab9074995b382e1f7
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-04-23 02:21:32 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        
+        String result = "Yes";
+        int index1 = 0;
+        int index2 = 0;
+        int index = 0; // goal index
+        
+        while(index < goal.length) {
+            if(index1 < cards1.length && cards1[index1].equals(goal[index])) {
+                index1++;
+                index++;
+            } else if(index2 < cards2.length && cards2[index2].equals(goal[index])) {
+                index2++;
+                index++;
+            } else {
+                result = "No";
+                break;
+            }
+        }
+        
+        return result;
+    }
+}
