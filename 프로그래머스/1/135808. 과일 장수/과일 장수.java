e5897f27da4effd5d0f67232c8be76eaa265a240
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-04-23 22:26:50 +0900

    [level 1] Title: 과일 장수, Time: 0.26 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,15 +1,22 @@
 import java.util.*;
-
 class Solution {
     public int solution(int k, int m, int[] score) {
-        int answer = 0;
-
-        Arrays.sort(score);
-
-        for(int i = score.length; i >= m; i -= m){
-            answer += score[i - m] * m;
+        // 1점부터 k점까지 점수로 분류
+        // 상자에 m개씩 담아 포장 -> 가장 낮은 점수 * m =  상자 가격
+        int result = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int item : score) {
+            list.add(item);
         }
-
-        return answer;
+        Collections.sort(list, Collections.reverseOrder());
+        
+        // System.out.println(list);
+        for(int i = (m-1); i < list.size(); i+=m) {
+            result += list.get(i) * m;
+        // System.out.println(result);
+        }
+        
+        return result;
+        
     }
 }
