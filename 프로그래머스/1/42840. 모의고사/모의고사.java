aa8d11a295a16ed8035e3b6bb083d2fcd7d7d126
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-04-24 21:19:11 +0900

    [level 1] Title: 모의고사, Time: 2.92 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,20 +1,47 @@
-import java.util.ArrayList;
+import java.util.*;
 class Solution {
-    public int[] solution(int[] answer) {
-        int[] a = {1, 2, 3, 4, 5};
-        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] score = new int[3];
-        for(int i=0; i<answer.length; i++) {
-            if(answer[i] == a[i%a.length]) {score[0]++;}
-            if(answer[i] == b[i%b.length]) {score[1]++;}
-            if(answer[i] == c[i%c.length]) {score[2]++;}
+    public int[] solution(int[] answers) {
+        // 1번 수포자 : 12345 반복 5
+        // 2번 수포자 : 21232425 반복 8
+        // 3번 수포자 : 3311224455 반복 10
+        
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] cnt = new int[3];
+        for (int i = 0; i < answers.length; i++) {
+            if(answers[i] == arr1[i% arr1.length]) {
+                cnt[0]++;
+            }
+            
+            if(answers[i] == arr2[i% arr2.length]) {
+                cnt[1]++;
+            }
+            
+            if(answers[i] == arr3[i% arr3.length]) {
+                cnt[2]++;
+            }
         }
-        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+        
+        int max = 0;
         ArrayList<Integer> list = new ArrayList<>();
-        if(maxScore == score[0]) {list.add(1);}
-        if(maxScore == score[1]) {list.add(2);}
-        if(maxScore == score[2]) {list.add(3);}
-        return list.stream().mapToInt(i->i.intValue()).toArray();
+        
+        for (int i = 0; i < cnt.length; i++) {
+            if (max < cnt[i]) {
+                max = cnt[i];
+            }
+        }
+        
+        for (int i = 0; i < cnt.length; i++) {
+            if(max == cnt[i]) {
+                list.add(i+1);
+            }
+        }
+        
+        
+        
+        // System.out.println(list);
+        int[] result = list.stream().mapToInt(Integer::intValue).toArray();
+        return result;
     }
 }
