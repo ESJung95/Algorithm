4fdc161c4056e05d62288d50cd0c8b95d9894eb5
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-04-24 21:38:37 +0900

    [level 1] Title: 소수 만들기, Time: 0.05 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,45 +1,28 @@
-import java.util.*;
+import java.util.Arrays;
 
-public class Solution {
-    
-    public static int[][] combinations(int[] nums, int r) {
-        List<int[]> result = new ArrayList<>();
-        
-        for(int i = 0; i < nums.length - 2; i++) {
-            for (int j = i+1; j < nums.length - 1; j++) {
-                for (int k = j+1; k < nums.length; k++) {
-                    int[] combination = new int[3];
-                    combination[0] = nums[i];
-                    combination[1] = nums[j];
-                    combination[2] = nums[k];
-                    result.add(combination);
+class Solution {
+
+
+
+    public int solution(int[] nums) {
+        int ans = 0;
+
+        for(int i = 0; i < nums.length - 2; i ++){
+            for(int j = i + 1; j < nums.length - 1; j ++){
+                for(int k = j + 1; k < nums.length; k ++ ){
+                    if(isPrime(nums[i] + nums[j] + nums[k])){
+                        ans += 1;  
+                    } 
                 }
             }
         }
-        
-        return result.toArray(new int[0][]);
+        return ans;
     }
-    
-    public int solution(int[] nums) {
-        int[][] combos = combinations(nums, 3);
-        int primeCount = 0;
-        
-        for (int[] combo : combos) {
-            int sum = combo[0] + combo[1] + combo[2];
-            if (isPrime(sum)) {
-                primeCount++;
-            }
+    public Boolean isPrime(int num){
+        int cnt = 0;
+        for(int i = 1; i <= (int)Math.sqrt(num); i ++){
+            if(num % i == 0) cnt += 1; 
         }
-        
-        return primeCount;
-    }
-    
-    // 소수 판별 함수
-    private boolean isPrime(int num) {
-        if (num < 2) return false;
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) return false;
-        }
-        return true;
+        return cnt == 1;
     }
 }
