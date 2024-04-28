2b81e3ab45b9658796da5726fbd6c59a71174c1b
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-04-28 17:10:36 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 3.16 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         
-        for (int i=0; i<queries.length; i++) {
-            int s = queries[i][0];
-            int e = queries[i][1];
-            int k = queries[i][2];
-            
-            for(int j=s; j<=e; j++) {
-               if(j%k==0){
-                    arr[j] += 1;   
-                } 
+        // 문제 이해를 못함 -> i는 index..
+        // query -> s, e, k
+        // s ~ e사이의 수 && k의 배수 = arr[i] + 1
+        
+        for (int i = 0; i < queries.length; i++) {
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) { // 수정된 부분: j로 변경
+                if (j % queries[i][2] == 0) {
+                    arr[j] += 1;
+                }
             }
         }
+        
         return arr;
     }
 }
