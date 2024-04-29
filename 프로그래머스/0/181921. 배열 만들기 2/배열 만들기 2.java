78d26472906b046e5c677f00c43cd539826f2cd9
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-04-29 20:00:00 +0900

    [level 0] Title: 배열 만들기 2, Time: 15.69 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.util.regex.*;
+class Solution {
+    public int[] solution(int l, int r) {
+        // l이상 r이하의 정수 중에서 숫자 0과 5로만 이루어진 정수를 오름차순으로 저장한 배열 return
+        ArrayList<Integer> list = new ArrayList<>();
+        // 정규식 패턴 생성
+        Pattern pattern = Pattern.compile("[05]+");
+        
+        for (int i = l; i <= r; i++) {
+            String num = String.valueOf(i);
+            Matcher matcher = pattern.matcher(num);
+            // 패턴과 일치하는지 확인
+            if (matcher.matches()) {
+                list.add(i);
+            }
+        }
+        
+        int[] none = new int[1];
+        if (list.size() == 0) {
+            none[0] = -1;
+            return none;
+        }
+        
+        int[] result = new int[list.size()];
+        int index = 0;
+        for (int item : list) {
+            result[index] = item;
+            index++;
+        }
+        
+        return result;
+    }
+}
