bc4d15e178008792a258462b13becb8a360d0074
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-05-08 21:11:53 +0900

    [level 1] Title: 체육복, Time: 0.23 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
 
 ### 성능 요약
 
-메모리: 77 MB, 시간: 0.34 ms
+메모리: 79.1 MB, 시간: 0.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 12일 21:59:27
+2024년 05월 08일 21:11:49
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,37 +1,52 @@
 import java.util.*;
+
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        
-        int result = (n - lost.length);
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] == reserve[j]) {
-                    result++;
-                    lost[i] = -5;
-                    reserve[j] = -5;
-                }
+        // 여분있는 학생
+        ArrayList<Integer> reserveList = new ArrayList<>();
+        for (int item : reserve) {
+            reserveList.add(item);
+        }
+
+        // 잃어버린 학생
+        ArrayList<Integer> lostList = new ArrayList<>();
+        for (int item : lost) {
+            lostList.add(item);
+        }
+
+        Collections.sort(lostList);
+        Collections.sort(reserveList);
+
+        // 여분의 체육복을 가진 학생이 도난당한 경우 처리
+        for (int i = 0; i < lostList.size(); i++) {
+            int lostStudent = lostList.get(i);
+            if (reserveList.contains(lostStudent)) {
+                lostList.remove(i);
+                reserveList.remove(Integer.valueOf(lostStudent));
+                i--;
             }
         }
-        
-        
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                
-                if (lost[i]+1 == reserve[j]) {
-                    result++;
-                    reserve[j] = -1;
-                    break;
-                } else if (lost[i]-1 == reserve[j]) {
-                    result++;
-                    reserve[j] = -1;
-                    break;
-                }
+
+        // 체육복을 빌려줄 수 있는 학생 찾기
+        for (int i = 0; i < lostList.size(); i++) {
+            int lostStudent = lostList.get(i);
+            int front = lostStudent - 1;
+            int back = lostStudent + 1;
+
+            if (reserveList.contains(front)) {
+                reserveList.remove(Integer.valueOf(front));
+                lostList.remove(i);
+                i--;
+            } else if (reserveList.contains(back)) {
+                reserveList.remove(Integer.valueOf(back));
+                lostList.remove(i);
+                i--;
             }
         }
-        
-        return result;
+
+        // 체육복을 가진 학생 수 계산
+        int count = n - lostList.size();
+
+        return count;
     }
 }
