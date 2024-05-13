a47c160804b2355c5471352a01e7fa1991b9a1d0
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-05-13 21:19:37 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.18 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -3,9 +3,10 @@ class Solution {
     public int[] solution(String[] wallpaper) {
         
         int[] result = new int[4];
-        ArrayList<Integer> row = new ArrayList<>();
-        ArrayList<Integer> col = new ArrayList<>();
+        List<Integer> row = new ArrayList<>();
+        List<Integer> col = new ArrayList<>();
         
+        // index == 행 , #위치 == 열
         for (int i = 0; i < wallpaper.length; i++) {
             for (int j = 0; j < wallpaper[i].length(); j++) {
                 if(wallpaper[i].charAt(j) == '#') {
@@ -15,10 +16,12 @@ class Solution {
             }
         }
         
-        result[0] = Collections.min(row);
-        result[1] = Collections.min(col);
+        result[0] = Collections.min(row); // 행
+        result[1] = Collections.min(col); // 열
+        
         result[2] = Collections.max(row) + 1;
-        result[3] =Collections.max(col) + 1;
+        result[3] = Collections.max(col) + 1;
+        
         return result;
     }
 }
