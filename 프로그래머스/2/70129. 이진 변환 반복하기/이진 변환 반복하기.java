a7cdff3d59ef43286e8f760bf39742c7ea76819d
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-05-17 18:32:14 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 9.73 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,39 +1,24 @@
-import java.util.*;
 class Solution {
     public int[] solution(String s) {
-
-        ArrayList<String> strList = new ArrayList<>();
-        for (String item : s.split("")) {
-            strList.add(item);
-        }
-
-        int[] result = new int[2];
+        
         int zeroCnt = 0;
         int cnt = 0;
-
-        while (!s.equals("1")) {
-            cnt++;
-            for (int i = 0; i < strList.size(); i++) {
-                if (strList.get(i).equals("0")) {
-                    zeroCnt++;
-                }
-            }
-
-            s = s.replace("0", "");
-            int c = s.length();
-            s = Integer.toBinaryString(c);
-
-            strList.clear();
-            for (String item : s.split("")) {
-                strList.add(item);
-            }
-
-
-        }
-
-        result[0] = cnt;
-        result[1] = zeroCnt;
         
+        // 길이를 구하면 사라진 0의 개수를 알 수 있다.
+        while(!s.equals("1")) {
+            
+            int length = s.length();
+            s = s.replace("0","");
+            zeroCnt += length - s.length();
+            
+            s = Integer.toBinaryString(s.length()); // 이진 변환
+            cnt++;
+            
+        }
+        
+        
+        int[] result = {cnt, zeroCnt};
         return result;
+        
     }
 }
