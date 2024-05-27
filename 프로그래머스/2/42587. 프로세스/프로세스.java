37fc74492f844cee8e85519349935dbb524a01da
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-05-27 21:47:34 +0900

    [level 2] Title: 프로세스, Time: 1.09 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,27 +1,35 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] priorities, int location) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); 
-        //우선순위 큐를 내림차순 정렬로 설정한다.
-        int answer = 0;
-
+        
+        List<Integer> queue = new LinkedList<>();
         for (int i = 0; i < priorities.length; i++) {
-            pq.add(priorities[i]);
+            queue.add(i);
         }
 
-        while (!pq.isEmpty()) {
-            for (int i = 0; i < priorities.length; i++) {
-                if (priorities[i] == pq.peek()) { //priorities의 값이 현재 오름차순 우선순위가 제일 높은 값과 같은지 확인한다.
-                    if (i == location) {
-                        answer++;
-                        return answer;
-                    }   
-                    pq.poll();
-                    answer++;
+        List<Integer> priorityQueue = new LinkedList<>();
+        for (int priority : priorities) {
+            priorityQueue.add(priority);
+        }
+
+        Collections.sort(priorityQueue, Collections.reverseOrder());
+
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int currentLocation = queue.remove(0);
+            int currentPriority = priorities[currentLocation];
+
+            if (currentPriority == priorityQueue.get(0)) {
+                priorityQueue.remove(0);
+                count++;
+                if (currentLocation == location) {
+                    return count;
                 }
+            } else {
+                queue.add(currentLocation);
             }
         }
-        return -1;
+
+        return count;
     }
 }
