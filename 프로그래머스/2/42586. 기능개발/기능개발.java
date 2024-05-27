9b61eb5147688ce5476678ef31b7734fb6f06320
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-05-27 21:41:26 +0900

    [level 2] Title: 기능개발, Time: 0.26 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,39 +1,42 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-
-        ArrayList<Integer> list = new ArrayList<>();
-        int[] day = new int[progresses.length];
         
-      
-        for(int i = 0; i < speeds.length; i++){
-            day[i]  = (100 - progresses[i]) / speeds[i];
-            if ((100 - progresses[i]) % speeds[i] != 0){
-                day[i]++;
-            }
+        // 100 - progresses -> 남은 일의 양 / speeds = 값을 올림하기
+        // 올림한 값을 배열에 담고 맨 앞부터 이하인 개수 cnt 
+        
+        int[] workRemain = new int[progresses.length];
+        for (int i = 0; i < progresses.length; i++) {
+            workRemain[i] = 100 - progresses[i];
         }
         
-        int days = day[0];
+        int[] workingTime = new int[speeds.length];
+        for (int i = 0; i < speeds.length; i++) {
+            workingTime[i] = (int)Math.ceil((double)workRemain[i] / speeds[i]);
+        }
+        
+        List<Integer> list = new ArrayList<>();
         int cnt = 1;
-        for(int i = 1; i < progresses.length; i++){
-            
-            if (days >= day[i]) {
+        int maxTime = workingTime[0];
+        
+        for (int i = 1; i < workingTime.length; i++) {
+            if (maxTime >= workingTime[i]) {
                 cnt++;
             } else {
                 list.add(cnt);
                 cnt = 1;
-                days = day[i];
+                maxTime = workingTime[i];
             }
         }
-        
         list.add(cnt);
         
+        int index = 0;
         int[] result = new int[list.size()];
-
-        for (int i = 0; i <list.size(); i++) {
-            result[i] = list.get(i);
+        for (int item : list) {
+            result[index] = item;
+            index++;
         }
-
+        
         return result;
     }
 }
