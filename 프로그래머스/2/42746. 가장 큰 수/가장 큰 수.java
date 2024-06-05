4f916d865e76dc63d5cdcf6404624d3e6e9b9d71
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-06-05 21:42:14 +0900

    [level 2] Title: 가장 큰 수, Time: 1.84 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,24 +1,27 @@
 import java.util.*;
-
 class Solution {
     public String solution(int[] numbers) {
-        String[] strNumbers = new String[numbers.length];
-    
+        String[] arr = new String[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            strNumbers[i] = String.valueOf(numbers[i]);
-        }
-      
-        Arrays.sort(strNumbers, (a, b) -> (b + a).compareTo(a + b));
-       
-        if (strNumbers[0].equals("0")) {
-            return "0";
-        }
-    
-        StringBuilder answer = new StringBuilder();
-        for (String number : strNumbers) {
-            answer.append(number);
+            arr[i] = String.valueOf(numbers[i]);
         }
         
-        return answer.toString();
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return (b + a).compareTo(a + b);
+            }
+        });
+        
+        if (arr[0].equals("0")) {
+            return "0";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (String s : arr) {
+            sb.append(s);
+        }
+        
+        return sb.toString();
     }
 }
