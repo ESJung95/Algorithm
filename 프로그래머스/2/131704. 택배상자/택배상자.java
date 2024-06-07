b6b67f8ce6ab30f9d71885081d8bb8552f1b2498
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-06-07 22:21:32 +0900

    [level 2] Title: 택배상자, Time: 71.52 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,37 +1,25 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] order) {
-        int result = 0;
-
-        int[] box = new int[order.length];
-        for (int i = 0; i < order.length; i++) {
-            box[i] = i + 1;
-        }
-
+        
         Stack<Integer> stack = new Stack<>();
-        int boxIndex = 0;
-        int orderIndex = 0;
-
-        while (orderIndex < order.length) {
-            if (boxIndex < order.length && box[boxIndex] == order[orderIndex]) {
+        int result = 0;
+        int index = 0;
+        for (int i = 1; i <= order.length; i++) { // box
+            if (order[index] != i) {
+                stack.push(i);
+            } else {
                 result++;
-                orderIndex++;
-                boxIndex++;
-            } else if (!stack.isEmpty() && stack.peek() == order[orderIndex]) {
+                index++;
+            }
+            
+            while(!stack.isEmpty() && stack.peek() == order[index]) {
                 stack.pop();
                 result++;
-                orderIndex++;
-            } else {
-                if (boxIndex < order.length) {
-                    stack.push(box[boxIndex]);
-                    boxIndex++;
-                } else {
-                    break;
-                }
+                index++;
             }
         }
-
+        
         return result;
     }
 }
