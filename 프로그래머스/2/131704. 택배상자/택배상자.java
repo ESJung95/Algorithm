2d4aeab4769c659da9e6bf3e0a802b5e7576130b
ESJung95 <155522048+ESJung95@users.noreply.github.com>
2024-06-07 18:55:17 +0900

    [level 2] Title: 택배상자, Time: 55.25 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -3,40 +3,35 @@ import java.util.*;
 class Solution {
     public int solution(int[] order) {
         int result = 0;
-        int n = order.length;
-        
-        int[] box = new int[n];
-        for (int i = 0; i < n; i++) {
+
+        int[] box = new int[order.length];
+        for (int i = 0; i < order.length; i++) {
             box[i] = i + 1;
         }
-        
+
         Stack<Integer> stack = new Stack<>();
-        int boxIndex = 0; // box 배열의 현재 인덱스
-        int orderIndex = 0; // order 배열의 현재 인덱스
-        
-        do {
-            if (boxIndex < n && box[boxIndex] == order[orderIndex]) {
-                // 박스에서 올바른 순서의 상자를 직접 찾음
+        int boxIndex = 0;
+        int orderIndex = 0;
+
+        while (orderIndex < order.length) {
+            if (boxIndex < order.length && box[boxIndex] == order[orderIndex]) {
                 result++;
                 orderIndex++;
                 boxIndex++;
             } else if (!stack.isEmpty() && stack.peek() == order[orderIndex]) {
-                // 스택의 상단에 올바른 순서의 상자가 있는 경우
                 stack.pop();
                 result++;
                 orderIndex++;
             } else {
-                // 박스에서 상자를 스택에 쌓음
-                if (boxIndex < n) {
+                if (boxIndex < order.length) {
                     stack.push(box[boxIndex]);
                     boxIndex++;
                 } else {
-                    // 더 이상 쌓을 상자가 없는 경우 중지
                     break;
                 }
             }
-        } while (orderIndex < n);
-        
+        }
+
         return result;
     }
 }
