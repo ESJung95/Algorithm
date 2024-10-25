c263af0ce24426537abce495c33b3971cd997442
Eun Sun <eunsunjj9501@gmail.com>
2024-10-25 22:40:26 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,12 @@
+-- 동물의 아이디, 이름, 중성화 여부 + 아이디 순으로 조회
+        
+SELECT ANIMAL_ID, NAME,
+    CASE
+        WHEN SEX_UPON_INTAKE LIKE 'Neutered%' THEN 'O'
+        WHEN SEX_UPON_INTAKE LIKE 'Spayed%' THEN 'O'
+        ELSE 'X'
+    END AS '중성화'
+    
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID ASC;
+
