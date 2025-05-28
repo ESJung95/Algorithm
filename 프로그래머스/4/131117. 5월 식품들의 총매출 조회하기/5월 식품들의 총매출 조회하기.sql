800cab3aeda38a39d615e53b3953647e33bdaf8a
Eun Sun <eunsunjj9501@gmail.com>
2025-05-28 09:40:46 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -0,0 +1,12 @@
+-- FOOD_PRODUCT와 FOOD_ORDER 테이블에서 생산일자가 2022년 5월인 식품들의 식품 ID, 식품 이름, 총매출을 조회하는 SQL문을 작성해주세요. -- 총매출을 기준으로 내림차순 정렬해주시고 총매출이 같다면 식품 ID를 기준으로 오름차순 정렬해주세요.
+
+SELECT 
+    p.PRODUCT_ID,
+    p.PRODUCT_NAME,
+    SUM(o.AMOUNT * p.PRICE) AS TOTAL_SALES
+FROM FOOD_PRODUCT p
+JOIN FOOD_ORDER o ON o.PRODUCT_ID = p.PRODUCT_ID
+WHERE DATE_FORMAT(o.PRODUCE_DATE, '%Y-%m') = '2022-05'
+GROUP BY o.PRODUCT_ID
+HAVING SUM(o.AMOUNT * p.PRICE) 
+ORDER BY TOTAL_SALES DESC, p.PRODUCT_ID ASC
